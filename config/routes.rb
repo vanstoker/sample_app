11c6a65d787c07009d6fffab810bce729cc21f5a
Ivan Nikonorov <vanstoker@mail.ru>
2018-05-05 20:14:43 +0300

    Can't pass the test

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
-  get '/help', to: 'static_pages/help'
-  get '/home', to: 'static_pages/home'
-  get '/about', to: 'static_pages/about'
-  get 'contact', to: 'static_pages/contact'
+  get '/static_pages/home', to: 'static_pages#home'
+  get '/static_pages/help', to: 'static_pages#help'
+  get '/static_pages/about', to: 'static_pages#about'
+  get '/static_pages/contact', to: 'static_pages#contact'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
