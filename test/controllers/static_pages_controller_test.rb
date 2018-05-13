01981acb12ea8fb32621b84884140f6ce77e43a9
Ivan Nikonorov <vanstoker@mail.ru>
2018-05-13 21:50:20 +0300

    Finished chapter five. I'm ready to the interesting task!

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -7,25 +7,25 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should get home" do
-    get root
+    get root_path
     assert_response :success
     assert_select "title", "Home | #{@base_title}"
   end
 
   test "should get help" do
-    get help_push
+    get help_path
     assert_response :success
     assert_select "title", "Help | #{@base_title}"
   end
 
   test "should get about" do
-    get about_push
+    get about_path
     assert_response :success
     assert_select "title", "About | #{@base_title}"
   end
 
   test "should get contact" do
-    get contact_push
+    get contact_path
     assert_response :success
     assert_select "title", "Contact | #{@base_title}"
   end
