0809c8fa5369313cb5a4e628bf783bcd5e246632
vanstoker <vanstoker@mail.ru>
2018-07-09 18:54:52 +0300

    Same problem after Listing 8.16

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-
-  def hello
-  	render html: "Have a good day!"
-  end
+  include SessionsHelper
 end
+
+  # def hello
+  #	render html: "Have a good day!"
+  # end
