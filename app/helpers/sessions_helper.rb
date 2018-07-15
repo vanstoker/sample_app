d021263fb7fce1e4d72241d71c358049a211b17a
vanstoker <vanstoker@mail.ru>
2018-07-15 16:47:33 +0300

    Implement advanced login

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -18,7 +18,7 @@ module SessionsHelper
 			@current_user ||= User.find_by(id: user_id)
 		elsif (user_id = cookies.signed[:user_id])
 			user = User.find_by(id: user_id)
-			gitif user && user.authenticated?(cookies[:remember_token])
+			if user && user.authenticated?(cookies[:remember_token])
 				log_in user
 				@current_user = user
 			end
@@ -41,5 +41,4 @@ module SessionsHelper
 		session.delete(:user_id)
 		@current_user = nil
 	end
-
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -30,7 +30,7 @@ class User < ApplicationRecord
   # Returns true if the given token matches the digest.
   def authenticated?(remember_token)
     return false if remember_digest.nil?
-    DCrypt::Password.new(remember_digest).is_password?(remember_token)
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
   end
 
   def forget
