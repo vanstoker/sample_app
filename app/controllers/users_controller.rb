6b5d85c6583d095f053baf3aa43a28a815ff3a0d
vanstoker <vanstoker@mail.ru>
2018-07-22 20:18:04 +0300

    RED test in Listing 13.35

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
-  before_action :correct_user, only: [:edit, :update]
-  before_action :admin_user, only: :destroy
+  # before_action :correct_user, only: [:edit, :update]
+  # before_action :admin_user, only: :destroy
   
   def index
     @users = User.paginate(page: params[:page])
@@ -9,6 +9,7 @@ class UsersController < ApplicationController
 
   def show
   	@user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
   	#debugger
   end
   
@@ -56,14 +57,14 @@ class UsersController < ApplicationController
 
   # Before filters
 
-  # Confirms a logged-in user.
-  def logged_in_user
-    unless logged_in?
-      store_location
-      flash[:danger] = "Please log in."
-      redirect_to login_url
-    end
-  end
+ #  # Confirms a logged-in user.
+ # def logged_in_user
+ #   unless logged_in?
+ #     store_location
+ #     flash[:danger] = "Please log in."
+ #     redirect_to login_url
+ #   end
+ # end
 
   # Confirms the correct user.
   def correct_user
